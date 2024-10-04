222629d2abef6e415991a3c004e8eb22e1bbbce7
lucifer-hell <luciferhell58@gmail.com>
2024-10-04 16:16:48 +0530

    yubi update

---
+++ src/main/java/Interviews/yubi/Round2.java
@@ -0,0 +1,9 @@
+package Interviews.yubi;
+
+public class Round2 {
+    public static void main(String[] args) {
+        //asked jump game problem
+        // asked book my show problem
+
+    }
+}
---
+++ src/main/java/Interviews/yubi/Round3.java
@@ -0,0 +1,9 @@
+package Interviews.yubi;
+
+public class Round3 {
+    public static void main(String[] args) {
+        // asked code smell issue
+        // asked hld design config service ( read - heavy )
+        // asked to various general behaivour and product based question
+    }
+}
