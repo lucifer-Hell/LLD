b98686efba7feaa173cefaa4fc2a4079d8a624c9
lucifer-hell <luciferhell58@gmail.com>
2024-12-05 13:13:50 +0530

    ADDED R2

---
+++ src/main/java/Interviews/Multiplier/Round2.java
@@ -0,0 +1,11 @@
+package Interviews.Multiplier;
+
+public class Round2 {
+    public static void main(String[] args) {
+        /*
+        * DB QUESTIONS - INDEXING
+        * HASHSET INDEXING , COMPARE FUNCTION QUESTIONS
+        * JAVA CODE CYCLE AND SPRING BOOT CYCLE BASED QUESTIONS
+        * */
+    }
+}
